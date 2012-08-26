d77e9e9c5e104e3ac48eb38ee869cc61c96f3f16
tomlee55 <s3262421@student.rmit.edu.au>
2012-08-26 23:10:43 +1000

    pdo file

---
+++ db_pdo.php
@@ -0,0 +1,19 @@
+<?php
+ /**
+  * MySQL connection check
+  *
+  * Checks the connection to the local install of MySQL
+  *
+  * @author Mengxiang Li <s3262421@student.rmit.edu.au>
+  * @version 1.0
+  * @package Connect
+  */
+  
+  define('DB_HOST', 'yallara.cs.rmit.edu.au');
+  define('DB_PORT', '50664');
+  define('DB_NAME', 'winestore');
+  define('DB_USER', 'winestore');
+  define('DB_PW', '19860309');
+  
+  $dsn = 'mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME;
+   
